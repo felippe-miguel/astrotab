d9c8d3c29be3cce53fb032309bad42ee331c2f0e
Felippe Miguel <felippe.miguel.silva@gmail.com>
2024-12-18 12:29:49 +0000

    test: add database connection tests for status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,28 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum;");
+  const updatedAt = new Date().toISOString();
 
-  console.log(result.rows);
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersion = databaseVersionResult.rows[0].server_version;
+
+  const resultMaxConnections = await database.query("SHOW max_connections;");
+  const maxConnections = resultMaxConnections.rows[0].max_connections;
+
+  const openedConnectionsResult = await database.query(
+    "SELECT COUNT(*)::int AS open_connections FROM pg_stat_activity",
+  );
+  const openedConnections = openedConnectionsResult.rows[0].open_connections;
+
   response.status(200).json({
-    message: "OK",
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        max_connections: Number(maxConnections),
+        opened_connections: openedConnections,
+        version: databaseVersion,
+      },
+    },
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -2,4 +2,23 @@ test("GET to api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
 
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  expect(responseBody.updated_at).toBeDefined();
+  expect(responseBody.dependencies).toBeDefined();
+  expect(responseBody.dependencies.database).toBeDefined();
+
+  const database = responseBody.dependencies.database;
+
+  expect(database.max_connections).toBeDefined();
+  expect(database.opened_connections).toBeDefined();
+  expect(database.version).toBeDefined();
+
+  expect(typeof database.max_connections).toBe("number");
+  expect(typeof database.opened_connections).toBe("number");
+  expect(typeof database.version).toBe("string");
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(parsedUpdatedAt).toBe(responseBody.updated_at);
 });
