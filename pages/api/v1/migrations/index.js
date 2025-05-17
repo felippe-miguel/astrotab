d4183f9850e4f055f846a73347c9c6da0a227296
Felippe Miguel <felippe.miguel.silva@gmail.com>
2025-05-17 14:16:46 +0000

    refactor: streamline database client creation and usage in migration API

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,9 +1,11 @@
 import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
+import database from "infra/database.js";
 
 export default async function migrations(request, response) {
+  const dbClient = await database.getNewClient();
   const defaultMigrationOptions = {
-    databaseUrl: process.env.DATABASE_URL,
+    dbClient: dbClient,
     dir: join("infra", "migrations"),
     direction: "up",
     migrationsTable: "pgmigrations",
@@ -13,6 +15,8 @@ export default async function migrations(request, response) {
   if (request.method === "GET") {
     const pendingMigrations = await migrationRunner(defaultMigrationOptions);
 
+    await dbClient.end();
+
     return response.status(200).json(pendingMigrations);
   }
 
@@ -21,6 +25,8 @@ export default async function migrations(request, response) {
       ...defaultMigrationOptions,
       dryRun: false,
     });
+
+    await dbClient.end();
 
     const statusCode = migratedMigrations.length > 0 ? 201 : 200;
 
